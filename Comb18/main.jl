29ac3b87dbabecbf4af7850ad8e12446ea4638e8
Kashish Goel <96175365+KashishGoel24@users.noreply.github.com>
2023-06-06 23:08:59 +0200

    Update main.jl

--- Comb18/main.jl
+++ Comb18/main.jl
@@ -3,9 +3,13 @@ using ProximalOperators
 using Random
 
 #intialising our problem
-global functions_I = 2
-global functions_K = 4
-global dims = 2
+global functions_I = 2    # m -  size of set I 
+global functions_K = 4    # p -  size of set K
+# global dims = 2
+# global dims_I = [dims1,dims2,...,dimsI]
+# global dims_K = [dims1,dims2,...,dimsK]
+global dims_I = [2,3]
+global dims_K = [1,2,3,2]
 global D = 40
 global iters = 5000
 global epsilon = 0.5
