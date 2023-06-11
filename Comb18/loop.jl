68e74fe5d886083910b3d5bf585e4be38b8828aa
Kashish Goel <96175365+KashishGoel24@users.noreply.github.com>
2023-06-11 21:09:02 +0200

    Update loop.jl
    Introduced Blocks

--- Comb18/loop.jl
+++ Comb18/loop.jl
@@ -1,10 +1,12 @@
 include("functions.jl")
 
 for j in 1:iters
+    global I_n = block_function(j,functions_I)
+    global K_n = block_function(j,functions_K)
     update_params(j)
-    if j%2==0
-        global minibatches = get_minibatch(j)
-    end
+    # if j%2==0
+    #     global minibatches = get_minibatch(j)
+    # end
     check_task_delay(j)
     define_tasks(j)
     compute(j, 1)
