79551e46ce9aeadc2e729f512fe2df4fca42f312
Kashish Goel <96175365+KashishGoel24@users.noreply.github.com>
2023-06-16 11:03:08 +0200

    Update main.jl

--- Comb18/main.jl
+++ Comb18/main.jl
@@ -1,22 +1,29 @@
 using LinearAlgebra
 using ProximalOperators
 using Random
+include("optimisation_problem2.jl")
 
 #intialising our problem
-# global functions_I = 2    # m -  size of set I 
-# global functions_K = 5    # p -  size of set K
-# global dims = 2
-# global dims_I = [dims1,dims2,...,dimsI]
-# global dims_K = [dims1,dims2,...,dimsK]
-# global dims_I = [2,2]
-# global dims_K = [2,2,2,2]
 global D = 40
 global iters = 5000
+# global iters = 10000
 global epsilon = 0.5
 
+global constant_g = []   # this is being defined if for generate_gamma the strategy being taken is generate_gamma_constant
+global constant_m = []   # this is being defined if for generate_mu the strategy being taken is generate_mu_constant
 
+for i in 1:functions_I
+    constant1 = epsilon + ((1/epsilon) * rand())
+    # constant1 = 2
+    append!(constant_g, constant1)
+end
+for i in 1:functions_K
+    constant2 = epsilon + ((1/epsilon) * rand())
+    # constant2 = 2
+    append!(constant_m, constant2)
+end
 
-include("optimisation_problem1.jl")
+include("optimisation_problem2.jl")
 include("variables.jl")
 include("loop.jl")
 
