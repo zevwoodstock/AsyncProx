75bae3ce04f2b612be25c9dd2455a27c86ace84d
Utkarsh Sharma <utkarsh.mask@gmail.com>
2023-07-05 21:34:03 +0530

    Update loop.jl

--- Comb18/loop.jl
+++ Comb18/loop.jl
@@ -1,12 +1,9 @@
 include("functions.jl")
 
 for j in 1:iters
-    global I_n = block_function(j,functions_I)
-    global K_n = block_function(j,functions_K)
+    global I_n = block_function(j,functions_I) # Keep M to be less than or equal to functions_I
+    global K_n = block_function(j,functions_K) # Keep M to be less than or equal to functions_K
     update_params(j)
-    # if j%2==0
-    #     global minibatches = get_minibatch(j)
-    # end
     check_task_delay(j)
     define_tasks(j)
     compute(j, 1)
@@ -15,3 +12,4 @@ for j in 1:iters
     write(j)
     update_vars(j)
 end
+record()
