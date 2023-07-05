a6e8e0cb33dba6ff57073a746210658812995628
Utkarsh Sharma <utkarsh.mask@gmail.com>
2023-07-05 21:33:39 +0530

    Update optimisation_problem_mixed.jl

--- Comb18/optimisation_problem_mixed.jl
+++ Comb18/optimisation_problem_mixed.jl
@@ -2,14 +2,47 @@ using LinearAlgebra
 using ProximalOperators
 using Random
 
+
 global functions_I = 3
 global functions_K = 5
 
 global dims_I = [2,2,3]
 global dims_K = [2,2,2,2,3]
+
+function get_block_cyclic(n::Int64, m::Int64 = 20, M::Int64 = 5) 
+    block_size = div(m, M)
+    start = (((n%M) - 1) * block_size) % m + 1
+    fin = start + block_size - 1
+
+    if n % M == 0
+        start = ((M - 1)* block_size)%m + 1
+        fin = max(fin, m)
+    end
+
+    arr = Int64[]
+    for i in start:fin
+        push!(arr, i)
+        # println(arr)
+    end
+    return arr
+end
+
+function generate_gamma_constant(i,j)
+    return constant_g[i]
+end
+
+function generate_mu_constant(k,j)
+    return constant_m[k]
+end
+
 global block_function = get_block_cyclic        #To be set by user
 global generate_gamma = generate_gamma_constant      #To be set by user
 global generate_mu = generate_mu_constant            #To be set by user
+
+#record_residual = 1 for storing ||x_{n+1} - x_n||^2
+global record_residual = true
+global record_func = false
+global record_dist = true
 
 global functions = []
 
@@ -55,5 +88,9 @@ append!(functions, [Linear([1,0])])
 append!(functions, [Linear([0,1])])
 append!(functions, [Linear([1,2,3])])
 
+global final_ans = [[0.0, 1.0], [0.5, 0.134], [0.73, 0.47, 0.2]]
+global f_ans = [0, 0, 0]
+global g_ans = [0,0, 0, 0.134, 2.27]
+
 #solution should be x1 = [0, 1x] , x2 =[0.5, 0.134] and x3 = [0.73, 0.47, 0.2]
-#modified to include L input as a function
+#modified
