e8dca1e4a3e413346ea2c934da64bd76d932c273
Kashish Goel <96175365+KashishGoel24@users.noreply.github.com>
2023-07-06 20:44:47 +0200

    Update loop.jl

--- Image_processing/loop.jl
+++ Image_processing/loop.jl
@@ -1,6 +1,11 @@
 include("functions.jl")
+include("problem.jl")
+
+global epoch_found = false
+global epoch_array = []
 
 for j in 1:iters
+    println("j = ",j)
     global I_n = block_function(j,functions_I,1)
     global K_n = block_function(j,functions_K,2)
     update_params(j)
@@ -11,4 +16,17 @@ for j in 1:iters
     calc_theta(j)
     write(j)
     update_vars(j)
+    if compute_epoch_bool == true
+        # if epoch_found == false
+        if compute_epoch()
+            global epoch_found = true
+            push!(epoch_array,j)
+            for i in 1:functions_I+functions_K
+                prox_call[i] = 0
+            end
+        end
+        # end
+    end
 end
+
+record()
