5e7650f28733c3718f9e13b0444487a4127818ba
Kashish Goel <96175365+KashishGoel24@users.noreply.github.com>
2023-07-06 20:44:24 +0200

    Update main.jl

--- Image_processing/main.jl
+++ Image_processing/main.jl
@@ -6,7 +6,7 @@ include("problem.jl")
 
 #intialising our problem
 global D = 40
-global iters = 5000
+global iters = 200
 global epsilon = 0.5
 
 global constant_g = []   # this is being defined if for generate_gamma the strategy being taken is generate_gamma_constant
@@ -30,19 +30,28 @@ include("loop.jl")
 println()
 # print("Final ans: ")
 
+println("reached here")
+
 x1 = res.x[iters][1]
 x2 = res.x[iters][2]
+
+
+println("reached here too")
 
 ret_image_left = matrix_to_image(vectorToImage(row1,col1,x1))
 ret_image_right = matrix_to_image(vectorToImage(row2,col2,x2))
 
+println("reached here tooooo")
 
 image_path_left = "/Users/kashishgoel/Desktop/Intern_2023/Image_processing/ret_image_left.jpeg"  # Replace with the desired path and filename for the image
 save(image_path_left, ret_image_left)  
 
+println("reached here tooooo")
+
 image_path_right = "/Users/kashishgoel/Desktop/Intern_2023/Image_processing/ret_image_right.jpeg"  # Replace with the desired path and filename for the image
 save(image_path_right, ret_image_right)  
 
+println("reached here tooooo")
 
 println(check_feasibility())
 println()
