80b7af1358100fb5f7b35b4fc6bf87bb46ab41ee
Kashish Goel <96175365+KashishGoel24@users.noreply.github.com>
2023-07-12 23:02:06 +0200

    Update main.jl

--- Image_processing/main.jl
+++ Image_processing/main.jl
@@ -18,8 +18,8 @@ for i in 1:functions_I
     append!(constant_g, constant1)
 end
 for i in 1:functions_K
-    constant2 = epsilon + ((1/epsilon) * rand())
-    # constant2 = 1
+    # constant2 = epsilon + ((1/epsilon) * rand())
+    constant2 = 1
     append!(constant_m, constant2)
 end
 
@@ -30,28 +30,33 @@ include("loop.jl")
 println()
 # print("Final ans: ")
 
-println("reached here")
+# println("reached here")
 
 x1 = res.x[iters][1]
 x2 = res.x[iters][2]
 
 
-println("reached here too")
+# println("reached here too")
 
 ret_image_left = matrix_to_image(vectorToImage(row1,col1,x1))
 ret_image_right = matrix_to_image(vectorToImage(row2,col2,x2))
 
-println("reached here tooooo")
+# println("reached here tooooo")
 
 image_path_left = "/Users/kashishgoel/Desktop/Intern_2023/Image_processing/ret_image_left.jpeg"  # Replace with the desired path and filename for the image
 save(image_path_left, ret_image_left)  
 
-println("reached here tooooo")
+# println("reached here tooooo")
 
 image_path_right = "/Users/kashishgoel/Desktop/Intern_2023/Image_processing/ret_image_right.jpeg"  # Replace with the desired path and filename for the image
 save(image_path_right, ret_image_right)  
 
-println("reached here tooooo")
+# println("reached here tooooo")
+println("mu = ",mu_array[1])
+println("theta = ",theta_main)
+println("sigma of noise = ",sigma_1)
+println("sigma of gaussian kernel = ", sigma_blur)
+
 
 println(check_feasibility())
 println()
