f4f167c8c0952f1851964a3331f54eeb7fb1d56b
Utkarsh Sharma <utkarsh.mask@gmail.com>
2023-11-08 17:51:56 +0530

    Update main.jl

--- Group sparse binary/main.jl
+++ Group sparse binary/main.jl
@@ -6,7 +6,7 @@ include("problem.jl")
 
 #intialising our problem
 global D = 40
-global iters = 1000
+global iters = 100
 global epsilon = 0.5
 
 global constant_g = []   # this is being defined if for generate_gamma the strategy being taken is generate_gamma_constant
