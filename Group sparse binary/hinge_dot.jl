f0f37d2c65d9f9578151283636ea147d6c34b9d4
Utkarsh Sharma <utkarsh.mask@gmail.com>
2023-11-28 21:58:13 +0530

    Update hinge_dot.jl
    Simulated 100 data centers - added latencies

--- Group sparse binary/hinge_dot.jl
+++ Group sparse binary/hinge_dot.jl
@@ -29,12 +29,21 @@ end
 # println("Result of HingeDot function call on x: ", result)
 
 function ProximalOperators.prox!(y, f::HingeDot, x, gamma)
-    mu = SqrNormL2(2)(f.mu[f.k])
+    datacenter_index = mapping_ping[f.k]
+    sleep_time = ping_array[datacenter_index]
+    n_error = randn()*0.005 #implies std_dev of normal error is 0.005 
+    sleep(abs(n_error) + sleep_time)
+    mu2 = SqrNormL2(2)(f.mu[f.k])
     Lx = Linear(f.mu[f.k])(x)
     p,v = prox(HingeLoss([f.beta[f.k]], 10), [Lx], gamma)
     p = p[1]
     p = p - Lx
     p = p*f.mu[f.k] #L*(v \in R^1) = v mu_k
-    y .= (x + (1/mu)*p)
+    y .= (x + (1/mu2)*p)
     return f(y)
 end
+
+#prox(f, x, gamma)
+# println("Prox of a pre-exisitng fn = ", prox(CubeNormL2(1), x, 1.0))
+# println("Prox of hinge_dot_function = ", prox(hinge_dot_function, x, 1.0))
+#We want to be able to call both preexisitng functions in ProximalOperators.jl and ones that we define ourselves.
