0c654b8b60df68f31badfc9795c7fe1baaf4ad79
Utkarsh Sharma <utkarsh.mask@gmail.com>
2024-07-09 21:31:50 +0530

    Update hyperparameters.jl

--- Group Sparse Binary Distributed/hyperparameters.jl
+++ Group Sparse Binary Distributed/hyperparameters.jl
@@ -1,11 +1,11 @@
-generate_gamma = generate_gamma_linear_decrease
+generate_gamma = generate_gamma_constant
 generate_mu = generate_mu_linear_decrease
 
 # For generate_gamma_constant
 params.constant_gamma = 1
 
 # For generate_mu_constant
-params.constant_mu = 0.35
+params.constant_mu = 0.1
 
 # For generate_gamma and mu random
 params.epsilon = 0.01
@@ -27,9 +27,9 @@ params.mu_start = []
 params.mu_end = []
 params.mu_step = []
 for k in 1:dimensions.num_func_K
-    append!(params.mu_start, 10)
-    append!(params.mu_end, 0.25)
-    append!(params.mu_step, 1)
+    append!(params.mu_start, 0.42)
+    append!(params.mu_end, 0.01)
+    append!(params.mu_step, 0.01)
 end
 
 # For generate_gamma_nonlinear_decrease
@@ -38,14 +38,14 @@ end
 params.gamma_a = []
 params.gamma_b = []
 for i in 1:dimensions.num_func_I
-    append!(params.gamma_a, 0.5)
-    append!(params.gamma_b, 5)
+    append!(params.gamma_a, 0.1)
+    append!(params.gamma_b, 0.1)
 end
 
 # For generate_mu_nonlinear_decrease
 params.mu_a = []
 params.mu_b = []
 for i in 1:dimensions.num_func_K
-    append!(params.mu_a, 0)
-    append!(params.mu_b, 5)
+    append!(params.mu_a, 0.1)
+    append!(params.mu_b, 0.25)
 end
