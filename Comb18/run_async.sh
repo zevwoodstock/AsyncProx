42fb984d61308b72c5b2fdbd2113015e2bbfe30f
CipherCook <utkarsh.mask@gmail.com>
2025-02-27 14:29:12 +0530

    Removed Comb18 as per earlier discussion on mail

--- Comb18/run_async.sh
+++
@@ -1,2 +0,0 @@
-export JULIA_NUM_THREADS=4
-/Applications/Julia-1.7.app/Contents/Resources/julia/bin/julia async_prox.jl
--- Comb18/start_repl.sh
+++
@@ -1,2 +0,0 @@
-export JULIA_NUM_THREADS=4
-exec '/Applications/Julia-1.7.app/Contents/Resources/julia/bin/julia'
